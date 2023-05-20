2ac8d9c0a23e865f46c2c88b32b2d679b03db2b6
faxe1008 <fabianblatz@gmail.com>
2023-05-20 11:00:46 +0200

    Add battery_sys for sampling battery voltage
    This patch adds a module to sample the battery voltage through the ADC.
    At the moment the ESP32 ADC does not support extra samples.

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,9 +1,15 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-
+#include "battery_sys.h"
 LOG_MODULE_REGISTER(app);
 
 void main(void)
 {
-    LOG_INF("Hello world");
+    uint8_t level;
+
+    battery_sys_init();
+    battery_sys_sample(&level);
+    LOG_INF("Battery Level: %u", level);
+
+
 }
